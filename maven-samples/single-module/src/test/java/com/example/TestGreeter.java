72b8157a697d569d05ecb3c698005ea5c47b22c0
nns15899 <nns15899@gmail.com>
2021-07-12 21:07:06 +0530

    made change in greeter echo

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thank you for making Nirbhay Happy";
+    String someone = "World, Thank you for making Nirbhay Happy Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
